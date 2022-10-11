afff1444d24e4d58914b8eaad06e4adbadb41e73
Mostafa <Mostafa0Mohamed2002@gmail.com>
2022-10-11 21:25:57 +0200

    add intent to move to login after animation ends

--- app/src/main/java/com/mostafadev/shoeappfirstsubmission/SplashScreen.kt
+++ app/src/main/java/com/mostafadev/shoeappfirstsubmission/SplashScreen.kt
@@ -1,11 +1,36 @@
 package com.mostafadev.shoeappfirstsubmission
 
+import android.animation.Animator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.mostafadev.shoeappfirstsubmission.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+
+        binding.lottiesplashscreen.addAnimatorListener(object : Animator.AnimatorListener{
+            override fun onAnimationStart(animation: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                val mainActivity= Intent(this@SplashScreen,MainActivity::class.java)
+                startActivity(mainActivity)
+                finish()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+
+        })
     }
 }
